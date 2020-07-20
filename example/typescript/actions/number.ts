f772d64f73ea2d3636ce4c430f7acf77ff09f140
Matt Davies <matt@teamdavies.co.uk>
2020-07-20 14:27:31 +0100

    add example comments update deps

--- example/typescript/actions/number.ts
+++ example/typescript/actions/number.ts
@@ -2,6 +2,7 @@ import { Context } from 'koa';
 import { BantamAction } from '@flmnt/bantam';
 
 class Number implements BantamAction {
+  // TRY THIS: curl --request GET 'http://localhost:3000/number/calculate/'
   getCalculate(ctx: Context): void {
     ctx.body = 2 + 2;
   }
--- example/typescript/actions/text.ts
+++ example/typescript/actions/text.ts
@@ -2,6 +2,7 @@ import { Context } from 'koa';
 import { BantamAction } from '@flmnt/bantam';
 
 class Text implements BantamAction {
+  // TRY THIS: curl --request GET 'http://localhost:3000/text/welcome-message/'
   getWelcomeMessage(ctx: Context): void {
     ctx.body = 'Hello Friend!';
   }
